e4ed35d6b0f2ecf63447a5fd4a5c6df40c2b2cb2
Ramon <Ramon>
2017-01-18 00:44:17 -0600

    Updated splash screen background

--- app/src/main/java/com/findigital/blossom/activities/SplashActivity.java
+++ app/src/main/java/com/findigital/blossom/activities/SplashActivity.java
@@ -3,12 +3,14 @@ package com.findigital.blossom.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.ActionMode;
 import android.view.View;
 import android.view.WindowManager;
 
+import com.findigital.blossom.R;
 import com.findigital.blossom.helpers.API;
 import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
@@ -27,12 +29,23 @@ public class SplashActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
 
-        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
-        Fabric.with(this, new Twitter(authConfig));
+        Handler handler = new Handler();
 
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
+        handler.postDelayed(runnable, 3000);
+
     }
+
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+            Fabric.with(getApplicationContext(), new Twitter(authConfig));
+
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    };
 }
