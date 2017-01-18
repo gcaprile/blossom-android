a60c4e18a7fb33857c6570867a0f46896cf0ec45
Ramon <Ramon>
2017-01-18 01:11:06 -0600

    Added menu access to Scholly view

--- app/src/main/java/com/findigital/blossom/fragments/SchollyFragmentActivity.java
+++ app/src/main/java/com/findigital/blossom/fragments/SchollyFragmentActivity.java
@@ -1,12 +1,15 @@
 package com.findigital.blossom.fragments;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ImageButton;
 
 import com.findigital.blossom.R;
 
@@ -20,6 +23,16 @@ public class SchollyFragmentActivity extends FragmentActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_scholly);
+
+        ImageButton btnMenu = (ImageButton) findViewById(R.id.btnMenu);
+        btnMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MenuFragmentActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.pull_in_right, R.anim.push_out_left);
+            }
+        });
 
         WebView webView = (WebView) findViewById(R.id.webView);
         webView.setWebViewClient(new WebViewClient() {
